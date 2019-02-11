7522be5a86797e37db0705f67c84e64bfa9f08a3
avellarmat <matheusavellar@ejcm.com.br>
2019-02-11 13:44:46 -0200

    Aprendendo ionic

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+  {
+    path: 'list',
+    loadChildren: './list/list.module#ListPageModule'
+  },
+  { path: 'pokemon', loadChildren: './pokemon/pokemon.module#PokemonPageModule' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/pokemon/pokemon.page.ts
@@ -0,0 +1,33 @@
+
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
+
+@Component({
+  selector: 'app-pokemon',
+  templateUrl: './pokemon.page.html',
+  styleUrls: ['./pokemon.page.scss'],
+})
+export class PokemonPage implements OnInit {
+  pokemons = [];
+  id:number;
+
+  constructor(public pkmnService: PokemonService) {
+      this.id=0;
+    }
+
+  ngOnInit() {
+  }
+
+  getPokemon(){
+  console.log("Entrei na função")
+  this.id++;
+  this.pkmnService.getPokemon(this.id)
+  .subscribe(
+    (res)=>{
+          this.pokemons.push(res);
+          console.log(res);
+      }
+    )
+  }
+
+}
